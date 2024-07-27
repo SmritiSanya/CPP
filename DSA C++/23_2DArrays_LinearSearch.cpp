c2c06f93a0cf65e7a3b703d17d02fc4bd784f306
SmritiSanya <94979806+SmritiSanya@users.noreply.github.com>
2024-07-27 13:21:34 +0530

    2D-Arrays and linear search

--- DSA C++/23_2DArrays_LinearSearch.cpp
+++ DSA C++/23_2DArrays_LinearSearch.cpp
@@ -1,19 +1,30 @@
 #include<iostream>
 using namespace std;
 
+bool isPresent(int arr[][4], int key,int i, int j){
+    for(int i=0;i<3;i++){
+        for(int j=0;j<4;j++){
+            if(arr[i][j]==key){
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
 int main(){
     //creating 2D Array
     //int arr[3][4]={1,2,3,4,5,6,7,8,9,10,11,12};
-    int arr[3][4]={{1,2,3,10},{4,5,6,11},{7,8,9,12}};
-
-    /*
+    //int arr[3][4]={{1,2,3,10},{4,5,6,11},{7,8,9,12}};
+    int arr[3][4];
+   
     //taking input: row wise
     for(int i=0;i<3;i++){
         for(int j=0;j<4;j++){
             cin>>arr[i][j];
         }
     }
-    */
+    
 
     /*
     //taking input: col wise
@@ -32,10 +43,19 @@ int main(){
         }
         cout<<endl;
     }
+
+    cout<<"Enter element to be search in Array: ";
+    int key;
+    cin>>key;
+    cout<<endl;
+    if(isPresent(arr,key,3,4)){
+        cout<<"Found the key";
+    }
+    else{
+        cout<<"Key is not present in the array";
+    }
     
-
-
-
+    
 
     return 0;
 }
